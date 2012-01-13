c40e90c0508bbda3ba578e6099790acef544356b
Thomas Volkmar Worm <tvw@s4r.de>
2012-01-13 15:33:39 +0100

    Increased number of records in test database.

--- db/migrate/002_fill_testdata.rb
+++ db/migrate/002_fill_testdata.rb
@@ -3,7 +3,7 @@ require 'model/testdata'
 class FillTestdata < ActiveRecord::Migration
   def self.up
     ActiveRecord::Base.transaction do
-      50_000.times do |n|
+      200_000.times do |n|
         puts "    ... #{n} records added (#{Time.now}) ..." if n % 10000 == 0 and n > 0
         Testdata.add_random_data(n)
       end
